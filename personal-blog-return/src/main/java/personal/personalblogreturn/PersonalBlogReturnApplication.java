d4147a8dc27779282367dda6c53b32afd92cef71
Ingram <59393351+ingram-13@users.noreply.github.com>
2020-01-14 23:46:13 +0800

    第一次提交代码

---
+++ personal-blog-return/src/main/java/personal/personalblogreturn/PersonalBlogReturnApplication.java
@@ -0,0 +1,13 @@
+package personal.personalblogreturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogReturnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBlogReturnApplication.class, args);
+	}
+
+}
---
+++ personal-blog-return/src/test/java/personal/personalblogreturn/PersonalBlogReturnApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.personalblogreturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogReturnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
